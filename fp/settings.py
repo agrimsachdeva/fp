24191d11772baefc9bc6772933eca40f91babbf9
Agrim <as2936@njit.edu>
2020-02-05 00:48:14 -0500

    export settings

--- fp/settings.py
+++ fp/settings.py
@@ -21,6 +21,9 @@ NEWSPIDER_MODULE = 'fp.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+FEED_FORMAT = 'jsonlines'
+FEED_URI = "products%(time).json"
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
